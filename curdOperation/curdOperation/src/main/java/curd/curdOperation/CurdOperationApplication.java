b194c52b1677c5704d0ef6582ac47b4a9997b68e
vikramkbgs <vikramkbgs@gmail.com>
2024-09-01 06:00:00 +0530

    curd revision

---
+++ curdOperation/curdOperation/src/main/java/curd/curdOperation/CurdOperationApplication.java
@@ -0,0 +1,13 @@
+package curd.curdOperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdOperationApplication.class, args);
+	}
+
+}
---
+++ curdOperation/curdOperation/src/test/java/curd/curdOperation/CurdOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package curd.curdOperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

42e14721b963e2c15647de7d6907ae943a7be0f9
vikramkbgs <vikramkbgs@gmail.com>
2024-09-03 16:44:08 +0530

    curd operation practice 03

--- curdOperation/curdOperation/src/main/java/controllers/homeController.java
+++ curdOperation/curdOperation/src/main/java/controllers/homeController.java
@@ -3,8 +3,9 @@ package controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class homeController {
 
     @GetMapping({"/", "home"})
